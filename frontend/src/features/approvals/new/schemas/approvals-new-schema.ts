8bc33b06bfe4d558255f4534215782d7479d5cc7
Yamazaki-katsunori <katunori0330@gmail.com>
2026-03-09 18:53:16 +0900

    feat commit: approvals/new schema and hook file create | sheard numberInput.ts create

--- frontend/src/features/approvals/new/schemas/approvals-new-schema.ts
+++ frontend/src/features/approvals/new/schemas/approvals-new-schema.ts
@@ -1,9 +1,12 @@
 import z from 'zod';
 
 export const ApprovalsNewFormSchema = z.object({
-  title: z.string(),
+  title: z.string().min(1, { error: 'タイトルは1文字以上入力してください' }),
   purchese_type: z.string(),
-  amount: z.string(),
+  amount: z
+    .string()
+    .min(1, { error: '購入金額は1円以上入力してください' })
+    .regex(/^\d+$/, '購入金額は数字のみ入力してください'),
   reason: z.string(),
 });
 
