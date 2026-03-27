6e72df3f8eacc96743f5eae370aa916eab58b72e
Yamazaki-katsunori <katunori0330@gmail.com>
2026-03-27 16:22:39 +0900

    fix commit: import pathの修正 | frontend/src/features/approvals/confirm/hooks/use-comfirm-approval-mutation.ts のファイル名称修正

--- frontend/src/features/approvals/confirm/hooks/use-confirm-approval-mutation.ts
+++ frontend/src/features/approvals/confirm/hooks/use-confirm-approval-mutation.ts
@@ -1,8 +1,8 @@
 'use client';
 
 import { useMutation } from '@tanstack/react-query';
-import { CreateApprovalRequestTypes } from '../../schemas/approvals-new-schema';
-import { confirmApprovalApi } from '../api/confirm-approval';
+import { CreateApprovalRequestTypes } from '@/features/approvals/schemas/approvals-new-schema';
+import { confirmApprovalApi } from '@/features/approvals/confirm/api/confirm-approval';
 
 export function useConfirmApprovalMutation() {
   return useMutation({
