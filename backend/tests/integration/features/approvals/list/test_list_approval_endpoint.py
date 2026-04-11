b5935e8154f0e3e25b4447ad8819fe2d2816954d
Yamazaki-katsunori <katunori0330@gmail.com>
2026-04-11 21:35:05 +0900

    test commit: backend: test_list_approval_endpoint.pyのテスト内容を作成/test_list_approval_repository.pyの取得したデータが最新の申請データ順としてsortされているかのassert内容を追加

--- backend/tests/integration/features/approvals/list/test_list_approval_endpoint.py
+++ backend/tests/integration/features/approvals/list/test_list_approval_endpoint.py
@@ -16,10 +16,38 @@ def test_get_approval_list_returns_paginated_response(
     assert response.status_code == 200
 
     body = response.json()
-    assert len(body["items"]) == 10
-    assert body["pagination"]["page"] == 1
-    assert body["pagination"]["per_page"] == 10
-    assert body["pagination"]["total"] == 12
-    assert body["pagination"]["total_pages"] == 2
-    assert body["pagination"]["has_prev"] is False
-    assert body["pagination"]["has_next"] is True
+    assert body["items"][0]["approval_user"] == seed_user.name
+    assert body["items"][0]["approval_status"] == submitted_status.name
+
+    assert body["pagination"] == {
+        "page": 1,
+        "per_page": 10,
+        "total": 12,
+        "total_pages": 2,
+        "has_prev": False,
+        "has_next": True,
+    }
+
+    assert [item["title"] for item in body["items"]] == [f"一覧確認用申請 {i}" for i in range(12, 2, -1)]
+
+
+def test_get_approval_list_returns_zero_paginated_response(
+    authenticated_client,
+):
+    """正常系: 申請一覧のendpoint test(0件取得時)"""
+
+    response = authenticated_client.get("/approvals?page=1&per_page=10")
+
+    assert response.status_code == 200
+
+    body = response.json()
+    assert body["items"] == []
+
+    assert body["pagination"] == {
+        "page": 1,
+        "per_page": 10,
+        "total": 0,
+        "total_pages": 0,
+        "has_prev": False,
+        "has_next": False,
+    }
--- backend/tests/integration/features/approvals/list/test_list_approval_repository.py
+++ backend/tests/integration/features/approvals/list/test_list_approval_repository.py
@@ -18,16 +18,12 @@ def test_find_all_returns_first_page_data(
 
     items = repository.get_approval_list(offset=0, limit=10)
     total = repository.approval_list_count_all()
-
-    print(f"items_Len: {len(items)}")
-    print(f"total: {total}")
-    print(f"items[0].title:  {items[0].title}")
-    print(f"items[1].title: {items[1].title}")
+    actual_titles = [item.title for item in items]
+    expected_titles = [f"一覧確認用申請 {i}" for i in range(12, 2, -1)]
 
     assert len(items) == 10
     assert total == 12
-    assert items[0].title == "一覧確認用申請 12"
-    assert items[1].title == "一覧確認用申請 11"
+    assert actual_titles == expected_titles
 
 
 def test_find_all_returns_second_page_data(
@@ -47,8 +43,9 @@ def test_find_all_returns_second_page_data(
 
     items = repository.get_approval_list(offset=10, limit=10)
     total = repository.approval_list_count_all()
+    actual_titles = [item.title for item in items]
+    expected_titles = [f"一覧確認用申請 {i}" for i in range(2, 0, -1)]
 
     assert len(items) == 2
     assert total == 12
-    assert items[0].title == "一覧確認用申請 2"
-    assert items[1].title == "一覧確認用申請 1"
+    assert actual_titles == expected_titles
